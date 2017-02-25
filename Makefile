fa8b164d00b2adadc999fe2cdf470aa397e89e4b
Kuroneko <kuroneko-git@sysadninjas.net>
2017-02-25 11:29:27 +1100

    migrate to cmake.  clean up crap.

--- Makefile
+++
@@ -1,8 +0,0 @@
-build:   lisa/lisaem_wx.cpp
-	./build.sh build 
-
-clean:   lisa/lisaem_wx.cpp
-	./build.sh clean
-
-install: lisa/lisaem_wx.cpp
-	./build.sh build  --install
--- include/machine.h
+++ include/machine.h
@@ -1,31 +1,23 @@
 #ifndef MACHINE_H
 #define MACHINE_H
 
-/* Adopt types from wx/defs.h to suit our definitions */
-#include <wx/defs.h>
+#include <stdint.h>
 
 #ifdef WORDS_BIGENDIAN
 #define BYTES_HIGHFIRST  1
 #endif
 
-#ifdef wxUint64
-typedef  wxUint64 uint64;
-#endif
-typedef  wxUint32 uint32;
-typedef  wxUint16 uint16;
-typedef  wxUint8 uint8;
+//typedef  uint64_t uint64;
+typedef  uint32_t uint32;
+typedef  uint16_t uint16;
+typedef  uint8_t  uint8;
 
-#ifdef wxInt64
-typedef  wxInt64 int64;
-#endif
-typedef  wxInt32 int32;
-typedef  wxInt16 int16;
-typedef  wxInt8 int8;
+typedef  int32_t int32;
+typedef  int16_t int16;
+typedef  int8_t int8;
 
-
-
-typedef int8 sint8;
-typedef int16 sint16;
-typedef int32 sint32;
+typedef int8_t sint8;
+typedef int16_t sint16;
+typedef int32_t sint32;
 
 #endif
